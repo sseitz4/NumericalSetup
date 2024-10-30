834631752cba7e97fa2792c6abb8d9c3e5bed7d2
sseitz4 <sebastian.seitz@manchester.ac.uk>
2024-10-30 15:53:05 +0000

    Update sol_struct.cpp

--- cppfuncs/sol_struct.cpp
+++ cppfuncs/sol_struct.cpp
@@ -2,12 +2,14 @@ typedef struct sol_struct
 {
  double* V;
  double* c;
+ double* labor;
 } sol_struct;
 
 double* get_double_p_sol_struct(sol_struct* x, char* name){
 
  if( strcmp(name,"V") == 0 ){ return x->V; }
  else if( strcmp(name,"c") == 0 ){ return x->c; }
+ else if( strcmp(name,"labor") == 0 ){ return x->labor; }
  else {return NULL;}
 
 }
