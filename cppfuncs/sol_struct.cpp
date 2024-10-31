7ef2c292b4788f22f1b493761ec70c999a08afab
sseitz4 <sebastian.seitz@manchester.ac.uk>
2024-10-31 15:55:59 +0000

    Update sol_struct.cpp

--- cppfuncs/sol_struct.cpp
+++ cppfuncs/sol_struct.cpp
@@ -1,13 +1,28 @@
 typedef struct sol_struct
 {
- double* V;
- double* c;
+ double* V_no;
+ double* c_no;
+ double* V_w;
+ double* c_w;
+ double* V_int;
+ int* g_Kids;
 } sol_struct;
 
 double* get_double_p_sol_struct(sol_struct* x, char* name){
 
- if( strcmp(name,"V") == 0 ){ return x->V; }
- else if( strcmp(name,"c") == 0 ){ return x->c; }
+ if( strcmp(name,"V_no") == 0 ){ return x->V_no; }
+ else if( strcmp(name,"c_no") == 0 ){ return x->c_no; }
+ else if( strcmp(name,"V_w") == 0 ){ return x->V_w; }
+ else if( strcmp(name,"c_w") == 0 ){ return x->c_w; }
+ else if( strcmp(name,"V_int") == 0 ){ return x->V_int; }
+ else {return NULL;}
+
+}
+
+
+int* get_int_p_sol_struct(sol_struct* x, char* name){
+
+ if( strcmp(name,"g_Kids") == 0 ){ return x->g_Kids; }
  else {return NULL;}
 
 }
