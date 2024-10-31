30b15292a8198332a0babfd28d39c775f610d34e
sseitz4 <sebastian.seitz@manchester.ac.uk>
2024-10-31 14:09:57 +0000

    Update utils.cpp

--- cppfuncs/utils.cpp
+++ cppfuncs/utils.cpp
@@ -8,7 +8,7 @@ namespace utils {
 
     double util(double cons, int number_kids, par_struct *par){
         // CRRA utility function
-        return pow((cons/(1.0 + 0.3 * number_kids)), 1.0-par->rho)/(1.0-par->rho);
+        return pow((cons/(1.0 + 0.3 * number_kids)), 1.0-par->rho)/(1.0-par->rho) + par-> util_kids * number_kids;
     }
     
 }
